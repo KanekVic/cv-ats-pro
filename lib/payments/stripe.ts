da3cac5375354354d7c71dead80ba783f5189c13
vickkttorlara.c <Vickkttorlara.c@gmail.com>
2026-05-17 18:07:27 -0600

    Fix: Update Stripe API version to 2026-04-22.dahlia

--- lib/payments/stripe.ts
+++ lib/payments/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: "2024-11-20.acacia",
+  apiVersion: "2026-04-22.dahlia",
 });
 
 export interface StripePrice {
